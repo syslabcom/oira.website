f919f84a18b6ac1663ebeed9c2555d98e71f52ec
Alexander Pilz <pilz@syslab.com>
2013-12-06 18:57:20 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.6.dev0'
+version = '1.0dev0'
 long_description = \
     open("README.txt").read() + "\n" + \
     open(os.path.join("docs", "HISTORY.txt")).read()
 
-setup(name='osha.campaigntoolkit',
+setup(name='oira.website',
       version=version,
-      description="EU-OSHA Campaign Toolkit",
+      description="EU-OSHA OiRA Website",
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
@@ -21,7 +21,7 @@ setup(name='osha.campaigntoolkit',
       url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['osha'],
+      namespace_packages=['oira'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
@@ -31,7 +31,6 @@ setup(name='osha.campaigntoolkit',
           'z3c.jbot',
           'Products.LoginLockout',
           'Products.PasswordStrength',
-          'mfabrik.webandmobile',
       ],
       extras_require={
           # list libs needed for testing this project
